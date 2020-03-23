68485699437446a7fd60f6cbf633a5350da39b81
Macen Vargas <mvargas9517@live.com>
2020-03-23 16:36:59 -0500

    Finished the layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: MainScreen(),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MainScreen extends StatelessWidget {
+
+  final List<String> title = ['Olga\nBoznanska', 'Viceroyalty\nof Peru', 'Stanislaw Ignacy\nWitkiewicz',];
+  final List<String> subtitle = ['Post-Impressionist\nPainting', 'Art from the\nViceroyalty of Peru', 'Stanislaw Ignacy\nWitkiewicz',];
+  List<Color> colors = [Colors.amber[50], Colors.grey[400], Colors.deepOrange[900]];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
                backgroundColor: Colors.white,
               leading: Padding(
                 padding: EdgeInsets.only(left: 12),
@@ -43,44 +46,98 @@ class MyApp extends StatelessWidget {
               ],
               
             ),
-            body: StartPage()
-        ),
+      body: Container(
+        height: double.infinity,
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+            itemCount: colors.length, itemBuilder: (context, index) {
+              return Stack(
+                children: <Widget>[
+                  
+                  Column(
+                      children: <Widget>[
+                      Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      child: Column(
+                          children: <Widget>[
+                            Container(
+                              color: colors[index],
+                              height: MediaQuery.of(context).size.height * 0.5,
+                              child: Center(child: 
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Row(
+                                       mainAxisAlignment: MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(title[index].toString(), style: TextStyle(color: Colors.black, fontSize: 36.0, fontWeight: FontWeight.bold),),
+                                            SizedBox(height: 4,),
+                                            Text(subtitle[index], style: TextStyle(color: Colors.black, fontSize: 21.0, fontWeight: FontWeight.bold),),
+                                            SizedBox(height: MediaQuery.of(context).size.height * 0.15,),
+                                            Container(
+                                              width: MediaQuery.of(context).size.width * 0.85,
+                                              height: 1,
+                                              color: Colors.black,
+                                            ),
+                                            SizedBox(height: 10,),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: <Widget>[
+                                                Text('Tickets Available',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  letterSpacing: 0.5,
+                                                ),),
+                                                SizedBox(width: MediaQuery.of(context).size.width * 0.25),
+                                                Row(
+                                                  children: <Widget>[
+                                                    Text('5 Out',
+                                                    style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      letterSpacing: 0.5,
+                                                    ),),
+                                                    Padding(
+                                                      padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                                                      child: Icon(Icons.brightness_1,
+                                                      size: 5,),
+                                                    ),
+                                                    Text('10 Dec 2018',
+                                                    style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      letterSpacing: 0.5,
+                                                    ),),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              )),
+                            ),
+                          ],
+                        ),
+                    ),
+                    ],
+                  ),
+                  Positioned(
+                    top: 30,
+                    right: 20,
+                    child: Icon(Ionicons.ios_arrow_round_forward,
+                    size: 55,),),
+                ],
+              );
+        }),
+      ),
     );
   }
 }
-
-class StartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-  child: new SingleChildScrollView(
-    child: new Column(
-      children: <Widget>[
-        Container(
-          color: Colors.white70,
-          height: 320,
-        ),
-        Container(
-          color: Colors.blue,
-          height: 320,
-        ),
-        Container(
-          color: Colors.white,
-          height: 320,
-        ),
-      ]
-    )
-  )
-);
-  }
-}
-
-class BoxCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
-
